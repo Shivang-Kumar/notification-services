d313a80808c14a81130ad50586eb23064b239da9
Shivang Kumar <2002shiva.2002@gmail.com>
2026-03-04 23:54:47 +0530

    Fixed postgres handshake issue

---
+++ src/main/java/com/practice/microservices/notification_service/repository/NotificationTemplateRepository.java
@@ -0,0 +1,14 @@
+package com.practice.microservices.notification_service.repository;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.practice.microservices.notification_service.models.NotificationTemplate;
+
+public interface NotificationTemplateRepository 
+        extends JpaRepository<NotificationTemplate, UUID> {
+
+    Optional<NotificationTemplate> 
+        findByTemplateCodeAndActiveTrue(String templateCode);
+}
